a1917dca648e36de5b200c442725a26edb6d955d
Arvin <gnimoaix857@gmail.com>
2022-01-01 00:22:54 +0700

    Replace 'var' usage with 'let' for safety reason

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,12 @@ const wallHeight = groundSide * 2
 
 // World axes
 function showWorldAxes(size, scene) {
-    var makeTextPlane = function(text, color, size) {
-        var dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 50, scene, true);
+    let makeTextPlane = function(text, color, size) {
+        let dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 50, scene, true);
         dynamicTexture.hasAlpha = true;
         dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color, "transparent", true);
-        var plane = BABYLON.MeshBuilder.CreatePlane("TextPlane", { size }, scene);
-        var planeMaterial = new BABYLON.StandardMaterial("TextPlaneMaterial", scene);
+        let plane = BABYLON.MeshBuilder.CreatePlane("TextPlane", { size }, scene);
+        let planeMaterial = new BABYLON.StandardMaterial("TextPlaneMaterial", scene);
         plane.material = planeMaterial;
         planeMaterial.backFaceCulling = false;
         planeMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
@@ -19,7 +19,7 @@ function showWorldAxes(size, scene) {
         return plane;
     };
 
-    var axisX = BABYLON.MeshBuilder.CreateLines(
+    let axisX = BABYLON.MeshBuilder.CreateLines(
         "axisX",
         {
             points: [BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)],
@@ -27,10 +27,10 @@ function showWorldAxes(size, scene) {
         scene,
     );
     axisX.color = new BABYLON.Color3(1, 0, 0);
-    var xChar = makeTextPlane("X", "red", size / 10);
+    let xChar = makeTextPlane("X", "red", size / 10);
     xChar.position = new BABYLON.Vector3(0.9 * size, -0.05 * size, 0);
 
-    var axisY = BABYLON.MeshBuilder.CreateLines(
+    let axisY = BABYLON.MeshBuilder.CreateLines(
         "axisY",
         {
             points: [BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)],
@@ -38,10 +38,10 @@ function showWorldAxes(size, scene) {
         scene,
     );
     axisY.color = new BABYLON.Color3(0, 1, 0);
-    var yChar = makeTextPlane("Y", "green", size / 10);
+    let yChar = makeTextPlane("Y", "green", size / 10);
     yChar.position = new BABYLON.Vector3(0, 0.9 * size, -0.05 * size);
 
-    var axisZ = BABYLON.MeshBuilder.CreateLines(
+    let axisZ = BABYLON.MeshBuilder.CreateLines(
         "axisZ",
         {
             points: [BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, -0.05 * size, size * 0.95), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, 0.05 * size, size * 0.95)],
@@ -49,7 +49,7 @@ function showWorldAxes(size, scene) {
         scene,
     );
     axisZ.color = new BABYLON.Color3(0, 0, 1);
-    var zChar = makeTextPlane("Z", "blue", size / 10);
+    let zChar = makeTextPlane("Z", "blue", size / 10);
     zChar.position = new BABYLON.Vector3(0, 0.05 * size, 0.9 * size);
 };
 
@@ -66,7 +66,7 @@ function createBoard(scene: BABYLON.Scene) {
     /// of each walls. They are formatted as following:
     ///
     /// [x-position, y-position, z-position, y-rotation]
-    var walls = new Map([
+    let walls = new Map([
         ['fWall', [0, wallHeight / 2 - 0.5, groundSide / 2, 0]],
         ['bWall', [0, wallHeight / 2 - 0.5, -groundSide / 2, 180]],
         ['lWall', [groundSide / 2, wallHeight / 2 - 0.5, 0, 90]],
@@ -82,13 +82,13 @@ function createBoard(scene: BABYLON.Scene) {
     mat.opacity = 0.98
 
     // Ground
-    var ground = BABYLON.MeshBuilder.CreateGround('ground', { width: groundSide, height: groundSide, subdivisions: groundSide }, scene)
+    let ground = BABYLON.MeshBuilder.CreateGround('ground', { width: groundSide, height: groundSide, subdivisions: groundSide }, scene)
     ground.material = mat
     ground.position = new BABYLON.Vector3(0, -0.5)
 
     // Walls
     for (let [name, data] of walls) {
-        var wall = BABYLON.MeshBuilder.CreatePlane(name, { width: groundSide, height: wallHeight }, scene)
+        let wall = BABYLON.MeshBuilder.CreatePlane(name, { width: groundSide, height: wallHeight }, scene)
         wall.position = new BABYLON.Vector3(data[0], data[1], data[2])
         wall.rotation = getRotateVector({ y: data[3] })
         wall.material = mat
@@ -98,35 +98,35 @@ function createBoard(scene: BABYLON.Scene) {
 class App {
     constructor() {
         // Create the canvas html element and attach it to the webpage
-        var canvas = document.createElement("canvas");
+        let canvas = document.createElement("canvas");
         canvas.style.width = "100%";
         canvas.style.height = "100%";
         canvas.id = "gameCanvas";
         document.body.appendChild(canvas);
 
         // Initialize babylon scene and engine
-        var engine = new BABYLON.Engine(canvas, true);
-        var scene = new BABYLON.Scene(engine);
+        let engine = new BABYLON.Engine(canvas, true);
+        let scene = new BABYLON.Scene(engine);
 
         showWorldAxes(10, scene)
 
         // Camera
-        var camera: BABYLON.ArcRotateCamera = new BABYLON.ArcRotateCamera("camera", 1, 1, 45, BABYLON.Vector3.Zero(), scene);
+        let camera: BABYLON.ArcRotateCamera = new BABYLON.ArcRotateCamera("camera", 1, 1, 45, BABYLON.Vector3.Zero(), scene);
         camera.attachControl(canvas, true);
 
         // Light
-        var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+        let light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
 
         // Board
         createBoard(scene)
 
         // Block
-        var block = BABYLON.MeshBuilder.CreateBox('block', { height: 1, width: 1, depth: 1, }, scene)
+        let block = BABYLON.MeshBuilder.CreateBox('block', { height: 1, width: 1, depth: 1, }, scene)
         block.enableEdgesRendering()
         block.edgesColor = new BABYLON.Color4(1, 1, 1, 1)
 
         // Material
-        var mat = new BABYLON.StandardMaterial('', scene)
+        let mat = new BABYLON.StandardMaterial('', scene)
         mat.diffuseColor = new BABYLON.Color3(1, 1, 0)
         block.material = mat
 
