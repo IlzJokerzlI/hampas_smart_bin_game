50c777f6b5a734ffbdec3b5f5e1a45415a23322a
Arvin <gnimoaix857@gmail.com>
2021-12-31 22:28:42 +0700

    BabylonJS import update and added axes line
    - Import BabylonJS with alias "BABYLON"
    - Added axes line

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,56 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
+import * as BABYLON from "@babylonjs/core";
+
+// World axis
+var showWorldAxis = function(size, scene) {
+    var makeTextPlane = function(text, color, size) {
+        var dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 50, scene, true);
+        dynamicTexture.hasAlpha = true;
+        dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color, "transparent", true);
+        var plane = BABYLON.MeshBuilder.CreatePlane("TextPlane", { size }, scene);
+        var planeMaterial = new BABYLON.StandardMaterial("TextPlaneMaterial", scene);
+        plane.material = planeMaterial;
+        planeMaterial.backFaceCulling = false;
+        planeMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+        planeMaterial.diffuseTexture = dynamicTexture;
+        return plane;
+    };
+
+    var axisX = BABYLON.MeshBuilder.CreateLines(
+        "axisX",
+        {
+            points: [BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)],
+        },
+        scene,
+    );
+    axisX.color = new BABYLON.Color3(1, 0, 0);
+    var xChar = makeTextPlane("X", "red", size / 10);
+    xChar.position = new BABYLON.Vector3(0.9 * size, -0.05 * size, 0);
+
+    var axisY = BABYLON.MeshBuilder.CreateLines(
+        "axisY",
+        {
+            points: [BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)],
+        },
+        scene,
+    );
+    axisY.color = new BABYLON.Color3(0, 1, 0);
+    var yChar = makeTextPlane("Y", "green", size / 10);
+    yChar.position = new BABYLON.Vector3(0, 0.9 * size, -0.05 * size);
+
+    var axisZ = BABYLON.MeshBuilder.CreateLines(
+        "axisZ",
+        {
+            points: [BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, -0.05 * size, size * 0.95), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, 0.05 * size, size * 0.95)],
+        },
+        scene,
+    );
+    axisZ.color = new BABYLON.Color3(0, 0, 1);
+    var zChar = makeTextPlane("Z", "blue", size / 10);
+    zChar.position = new BABYLON.Vector3(0, 0.05 * size, 0.9 * size);
+};
 
 class App {
     constructor() {
@@ -13,13 +62,15 @@ class App {
         document.body.appendChild(canvas);
 
         // initialize babylon scene and engine
-        var engine = new Engine(canvas, true);
-        var scene = new Scene(engine);
+        var engine = new BABYLON.Engine(canvas, true);
+        var scene = new BABYLON.Scene(engine);
 
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+        showWorldAxis(10, scene)
+
+        var camera: BABYLON.ArcRotateCamera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, BABYLON.Vector3.Zero(), scene);
         camera.attachControl(canvas, true);
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+        var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+        var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
 
         // hide/show the Inspector
         window.addEventListener("keydown", (ev) => {
