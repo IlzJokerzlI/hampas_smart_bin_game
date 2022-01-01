414989e6e2a8dc146f88ca85a367d0bd07ce41ad
Arvin <gnimoaix857@gmail.com>
2022-01-01 18:24:40 +0700

    Refactor world axes into utils

--- src/app.ts
+++ src/app.ts
@@ -1,58 +1,9 @@
 import * as BABYLON from "babylonjs"
 import { GameBoard } from "./app/game-board"
-import { Rad } from './app/utils'
+import { Rad, WorldAxes } from './app/utils'
 
 const groundSide = 11
 const wallHeight = groundSide * 2
-
-// World axes
-function showWorldAxes(size, scene) {
-    let makeTextPlane = function(text, color, size) {
-        let dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 50, scene, true)
-        dynamicTexture.hasAlpha = true
-        dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color, "transparent", true)
-        let plane = BABYLON.MeshBuilder.CreatePlane("TextPlane", { size }, scene)
-        let planeMaterial = new BABYLON.StandardMaterial("TextPlaneMaterial", scene)
-        plane.material = planeMaterial
-        planeMaterial.backFaceCulling = false
-        planeMaterial.specularColor = new BABYLON.Color3(0, 0, 0)
-        planeMaterial.diffuseTexture = dynamicTexture
-        return plane
-    }
-
-    let axisX = BABYLON.MeshBuilder.CreateLines(
-        "axisX",
-        {
-            points: [BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)],
-        },
-        scene,
-    )
-    axisX.color = new BABYLON.Color3(1, 0, 0)
-    let xChar = makeTextPlane("X", "red", size / 10)
-    xChar.position = new BABYLON.Vector3(0.9 * size, -0.05 * size, 0)
-
-    let axisY = BABYLON.MeshBuilder.CreateLines(
-        "axisY",
-        {
-            points: [BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)],
-        },
-        scene,
-    )
-    axisY.color = new BABYLON.Color3(0, 1, 0)
-    let yChar = makeTextPlane("Y", "green", size / 10)
-    yChar.position = new BABYLON.Vector3(0, 0.9 * size, -0.05 * size)
-
-    let axisZ = BABYLON.MeshBuilder.CreateLines(
-        "axisZ",
-        {
-            points: [BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, -0.05 * size, size * 0.95), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, 0.05 * size, size * 0.95)],
-        },
-        scene,
-    )
-    axisZ.color = new BABYLON.Color3(0, 0, 1)
-    let zChar = makeTextPlane("Z", "blue", size / 10)
-    zChar.position = new BABYLON.Vector3(0, 0.05 * size, 0.9 * size)
-}
 
 class App {
     constructor() {
@@ -66,9 +17,8 @@ class App {
         // Initialize babylon scene and engine
         let engine = new BABYLON.Engine(canvas, true)
         let scene = new BABYLON.Scene(engine)
+        let worldAxes: WorldAxes | null = null
         scene.collisionsEnabled = true
-
-        showWorldAxes(10, scene)
 
         // Camera
         let camera: BABYLON.ArcRotateCamera = new BABYLON.ArcRotateCamera("camera", Rad(270), 0.5, 45, BABYLON.Vector3.Zero(), scene)
@@ -91,14 +41,21 @@ class App {
         mat.diffuseColor = new BABYLON.Color3(1, 1, 0)
         block.material = mat
 
-        // Hide/show the Inspector
         window.addEventListener("keydown", (ev) => {
-            // Shift+Ctrl+Alt+I
-            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.key === 'I') {
-                if (scene.debugLayer.isVisible()) {
-                    scene.debugLayer.hide()
-                } else {
-                    scene.debugLayer.show()
+            if (ev.ctrlKey && ev.shiftKey && ev.altKey) {
+                if (ev.key === 'I') { // Hide/show the Inspector (Ctrl+Shift+Alt+I)
+                    if (scene.debugLayer.isVisible()) {
+                        scene.debugLayer.hide()
+                    } else {
+                        scene.debugLayer.show()
+                    }
+                } else if (ev.key === 'X') { // Hide/show the world axes (Ctrl+Shift+Alt+X)
+                    if (worldAxes === null) {
+                        worldAxes = new WorldAxes(scene, {})
+                    } else {
+                        worldAxes.dispose()
+                        worldAxes = null
+                    }
                 }
             }
 
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,11 +1,77 @@
 import * as BABYLON from 'babylonjs'
 
 // Get radiant value from degree
-export function Rad(val: number): number {
-    return (val == 0) ? 0 : val * Math.PI / 180
-}
+export function Rad(val: number): number { return (val == 0) ? 0 : val * Math.PI / 180 }
 
 // Vector for rotation with degree as unit
 export function RadRotVect({ x = 0, y = 0, z = 0 }): BABYLON.Vector3 {
     return new BABYLON.Vector3(Rad(x), Rad(y), Rad(z))
+}
+
+// World axes
+export class WorldAxes {
+    private _scene: BABYLON.Scene
+    private _AxesLines: Array<BABYLON.LinesMesh> = []
+
+    dispose() {
+        this._AxesLines.forEach((v) => {
+            v.dispose()
+        })
+    }
+
+    constructor(scene: BABYLON.Scene, { size = 10 }) {
+        this._scene = scene
+        let makeTextPlane = function(text: string, color: string, size: number) {
+            let dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 50, scene, true)
+            dynamicTexture.hasAlpha = true
+            dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color, "transparent", true)
+            let plane = BABYLON.MeshBuilder.CreatePlane("TextPlane", { size }, scene)
+            let planeMaterial = new BABYLON.StandardMaterial("TextPlaneMaterial", scene)
+            plane.material = planeMaterial
+            planeMaterial.backFaceCulling = false
+            planeMaterial.specularColor = new BABYLON.Color3(0, 0, 0)
+            planeMaterial.diffuseTexture = dynamicTexture
+            return plane
+        }
+
+        let axisX = BABYLON.MeshBuilder.CreateLines(
+            "axisX",
+            {
+                points: [BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)],
+            },
+            scene,
+        )
+        axisX.color = new BABYLON.Color3(1, 0, 0)
+        let xChar = makeTextPlane("X", "red", size / 10)
+        xChar.position = new BABYLON.Vector3(0.9 * size, -0.05 * size, 0)
+        xChar.parent = axisX
+
+        let axisY = BABYLON.MeshBuilder.CreateLines(
+            "axisY",
+            {
+                points: [BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)],
+            },
+            scene,
+        )
+        axisY.color = new BABYLON.Color3(0, 1, 0)
+        let yChar = makeTextPlane("Y", "green", size / 10)
+        yChar.position = new BABYLON.Vector3(0, 0.9 * size, -0.05 * size)
+        yChar.parent = axisY
+
+        let axisZ = BABYLON.MeshBuilder.CreateLines(
+            "axisZ",
+            {
+                points: [BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, -0.05 * size, size * 0.95), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, 0.05 * size, size * 0.95)],
+            },
+            scene,
+        )
+        axisZ.color = new BABYLON.Color3(0, 0, 1)
+        let zChar = makeTextPlane("Z", "blue", size / 10)
+        zChar.position = new BABYLON.Vector3(0, 0.05 * size, 0.9 * size)
+        zChar.parent = axisZ
+
+        this._AxesLines.push(axisX)
+        this._AxesLines.push(axisY)
+        this._AxesLines.push(axisZ)
+    }
 }
