2f08fa2eb72f86931430002aa38fe20031576f93
Arvin <gnimoaix857@gmail.com>
2022-01-02 19:04:30 +0700

    Added collision and fall
    - Added block collision with ammo.js physic engine
    - Added block fall
    - Simple working tetris

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import * as BABYLON from "babylonjs"
 import { GameBoard } from "./app/game-board"
 import { Rad, WorldAxes } from './app/utils'
+import Ammo from 'ammojs-typed'
 
 const groundSide = 11
 const wallHeight = groundSide * 2
 
 class App {
-    constructor() {
+    ammo: typeof Ammo
+    constructor(ammo: typeof Ammo) {
+        this.ammo = ammo
+
         // Create the canvas html element and attach it to the webpage
         let canvas = document.createElement("canvas")
         canvas.style.width = "100%"
@@ -19,6 +23,7 @@ class App {
         let scene = new BABYLON.Scene(engine)
         let worldAxes: WorldAxes | null = null
         scene.collisionsEnabled = true
+        scene.enablePhysics(new BABYLON.Vector3(0, 0, 0), new BABYLON.AmmoJSPlugin(true, this.ammo))
 
         // Camera
         let camera: BABYLON.ArcRotateCamera = new BABYLON.ArcRotateCamera("camera", Rad(270), 0.5, 45, BABYLON.Vector3.Zero(), scene)
@@ -35,6 +40,19 @@ class App {
         let block = BABYLON.MeshBuilder.CreateBox('block', { height: 1, width: 1, depth: 1, }, scene)
         block.enableEdgesRendering()
         block.edgesColor = new BABYLON.Color4(1, 1, 1, 1)
+        block.position = new BABYLON.Vector3(0, 10, 0)
+        block.checkCollisions = true;
+        block.ellipsoid = new BABYLON.Vector3(0.5, 0.5, 0.5)
+        block.physicsImpostor = new BABYLON.PhysicsImpostor(block, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, friction: 0, restitution: 0 })
+
+        // Block
+        let block2 = BABYLON.MeshBuilder.CreateBox('block', { height: 1, width: 1, depth: 1, }, scene)
+        block2.enableEdgesRendering()
+        block2.edgesColor = new BABYLON.Color4(1, 1, 1, 1)
+        block2.position = new BABYLON.Vector3(0, 3, 0)
+        block2.ellipsoid = new BABYLON.Vector3(0.5, 0.5, 0.5)
+        block2.checkCollisions = true
+        block2.physicsImpostor = new BABYLON.PhysicsImpostor(block2, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, friction: 0, restitution: 0 }, scene)
 
         // Material
         let mat = new BABYLON.StandardMaterial('', scene)
@@ -91,29 +109,45 @@ class App {
 
             // Block movement
             if (ev.key === 'w' && block.position.z < groundSide / 2 - 1) { // Front
-                block.position.z += 1
+                block.moveWithCollisions(new BABYLON.Vector3(0, 0, 1))
             } else if (ev.key === 's' && block.position.z > -groundSide / 2 + 1) { // Back
-                block.position.z -= 1
+                block.moveWithCollisions(new BABYLON.Vector3(0, 0, -1))
             } else if (ev.key === 'a' && block.position.x > -groundSide / 2 + 1) { // Left
-                block.position.x -= 1
+                block.moveWithCollisions(new BABYLON.Vector3(-1, 0, 0))
             } else if (ev.key === 'd' && block.position.x < groundSide / 2 - 1) { // Right
-                block.position.x += 1
-            } else if (!ev.shiftKey && ev.key === ' ' && block.position.y > 0) { // Down
-                block.position.y -= 1
+                block.moveWithCollisions(new BABYLON.Vector3(1, 0, 0))
+            } else if (!ev.shiftKey && ev.key === ' ' && block.position.y > 0.5) { // Down
+                block.moveWithCollisions(new BABYLON.Vector3(0, -1, 0))
             } else if (ev.shiftKey && ev.key === ' ' && block.position.y < wallHeight - 1.5) { // Up
-                block.position.y += 1
+                block.moveWithCollisions(new BABYLON.Vector3(0, 1, 0))
             }
+            block.position = new BABYLON.Vector3(Math.round(block.position.x), Math.round(block.position.y), Math.round(block.position.z))
         })
+
+        setInterval(() => {
+            if (!block.collider ?.collisionFound) {
+                block.moveWithCollisions(new BABYLON.Vector3(0, -1, 0))
+            }
+        }, 1000)
 
         // Run the main render loop
         engine.runRenderLoop(() => {
-
             // Prevent camera going under ground level
             if (camera.position.y < 0) {
                 camera.beta = Rad(90)
             }
             scene.render()
         })
+
     }
 }
-new App()
+
+// Entry point
+async function main() {
+    const ammo = await Ammo()
+
+    // Start App
+    new App(ammo)
+}
+
+main()
