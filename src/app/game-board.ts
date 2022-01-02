41a4f10093a93f7b17f6556d963555fae72e8bad
Arvin <gnimoaix857@gmail.com>
2022-01-02 20:27:29 +0700

    Fix block not colliding with ground

--- src/app/game-board.ts
+++ src/app/game-board.ts
@@ -3,7 +3,7 @@ import { GridMaterial } from 'babylonjs-materials'
 import { RadRotVect } from './utils'
 
 interface Wall {
-    name: string
+    side: string
     xPos: number
     yPos: number
     zPos: number
@@ -22,10 +22,10 @@ export class GameBoard {
         this._create()
     }
 
-    private _createGridMat(): GridMaterial {
+    private _createGridMat(name: string, { gridOffset = new BABYLON.Vector3(0.5, 0, 0.5) }): GridMaterial {
         const mat = new GridMaterial('grid', this._scene)
         mat.majorUnitFrequency = 0
-        mat.gridOffset = new BABYLON.Vector3(0.5, 0, 0.5)
+        mat.gridOffset = gridOffset
         mat.lineColor = new BABYLON.Color3(1.0, 1.0, 1.0)
         mat.mainColor = new BABYLON.Color3(1.0, 1.0, 1.0)
         mat.opacity = 0.99
@@ -33,21 +33,22 @@ export class GameBoard {
     }
 
     private _create() {
-        const mat = this._createGridMat()
         const wallsData = [
-            <Wall>{ name: 'fWall', xPos: 0, yPos: this._wallHeight / 2 - 0.5, zPos: this._groundSide / 2, yRot: 0 },
-            <Wall>{ name: 'bWall', xPos: 0, yPos: this._wallHeight / 2 - 0.5, zPos: -this._groundSide / 2, yRot: 180 },
-            <Wall>{ name: 'lWall', xPos: -this._groundSide / 2, yPos: this._wallHeight / 2 - 0.5, zPos: 0, yRot: 270 },
-            <Wall>{ name: 'rWall', xPos: this._groundSide / 2, yPos: this._wallHeight / 2 - 0.5, zPos: 0, yRot: 90 },
+            <Wall>{ side: 'f', xPos: 0, yPos: this._wallHeight / 2 - 0.5, zPos: this._groundSide / 2, yRot: 0 },
+            <Wall>{ side: 'b', xPos: 0, yPos: this._wallHeight / 2 - 0.5, zPos: -this._groundSide / 2, yRot: 180 },
+            <Wall>{ side: 'l', xPos: -this._groundSide / 2, yPos: this._wallHeight / 2 - 0.5, zPos: 0, yRot: 270 },
+            <Wall>{ side: 'r', xPos: this._groundSide / 2, yPos: this._wallHeight / 2 - 0.5, zPos: 0, yRot: 90 },
         ]
 
         // Ground
-        const ground = BABYLON.MeshBuilder.CreateGround('ground', { width: this._groundSide, height: this._groundSide, subdivisions: this._groundSide }, this._scene)
-        ground.material = mat
+        const ground = BABYLON.MeshBuilder.CreatePlane('ground', { size: this._groundSide, sideOrientation: BABYLON.Mesh.DOUBLESIDE }, this._scene)
+        ground.material = this._createGridMat('groundMat', { gridOffset: new BABYLON.Vector3(0.5, 0.5, 0) })
         ground.position = new BABYLON.Vector3(0, -0.5, 0)
+        ground.rotation = RadRotVect({ x: 90 })
+        ground.checkCollisions = true
 
         // Lid
-        const lid = BABYLON.MeshBuilder.CreatePlane('lid', { width: this._groundSide, height: this._groundSide }, this._scene)
+        const lid = BABYLON.MeshBuilder.CreatePlane('lid', { size: this._groundSide }, this._scene)
         lid.position = new BABYLON.Vector3(0, this._wallHeight - 0.5, 0)
         lid.rotation = RadRotVect({ x: 90 })
         lid.checkCollisions = true
@@ -56,14 +57,14 @@ export class GameBoard {
         // Walls
         wallsData.forEach((v) => {
             // Graphical wall
-            let wall = BABYLON.MeshBuilder.CreatePlane(v.name, { width: this._groundSide, height: this._wallHeight }, this._scene)
+            let wall = BABYLON.MeshBuilder.CreatePlane(v.side + 'Wall', { width: this._groundSide, height: this._wallHeight }, this._scene)
             wall.position = new BABYLON.Vector3(v.xPos, v.yPos, v.zPos)
             wall.rotation = RadRotVect({ y: v.yRot })
-            wall.material = mat
+            wall.material = this._createGridMat('wallMat', { gridOffset: new BABYLON.Vector3(0.5, 0, 0) })
 
             // Collision
             if (this._scene.collisionsEnabled) {
-                let wallCollision = BABYLON.MeshBuilder.CreatePlane(v.name, { width: this._groundSide, height: this._wallHeight, sideOrientation: BABYLON.Mesh.BACKSIDE }, this._scene)
+                let wallCollision = BABYLON.MeshBuilder.CreatePlane(v.side + 'Mat', { width: this._groundSide, height: this._wallHeight, sideOrientation: BABYLON.Mesh.BACKSIDE }, this._scene)
                 wallCollision.position = new BABYLON.Vector3(v.xPos, v.yPos, v.zPos)
                 wallCollision.rotation = RadRotVect({ y: v.yRot })
                 wallCollision.checkCollisions = true
