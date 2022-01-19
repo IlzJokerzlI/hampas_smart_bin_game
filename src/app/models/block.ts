775551d727b912ba3342811af8f3526ed54e1950
gadtardipw <gadtardi@gmail.com>
2022-01-19 18:52:34 +0700

    added textures

--- src/app/models/block.ts
+++ src/app/models/block.ts
@@ -7,6 +7,7 @@ interface Params {
     size: BABYLON.Vector3
     spawnPoint: BABYLON.Vector3
     weight: double
+    
 }
 
 export class Block {
@@ -15,6 +16,9 @@ export class Block {
     constructor(params: Params) {
         const size = params.size
 
+        const trashTexture = new BABYLON.StandardMaterial("trashTexture", params.scene);
+        trashTexture.diffuseTexture = new BABYLON.Texture("/assets/textures/trash.jpg", params.scene);
+
         this.shape = BABYLON.MeshBuilder.CreateBox('this.shape', { width: size.x, height: size.y, depth: size.z }, params.scene)
         this.shape.enableEdgesRendering()
         this.shape.edgesColor = new BABYLON.Color4(1, 1, 1, 1)
@@ -22,7 +26,8 @@ export class Block {
         this.shape.position = params.spawnPoint
         this.shape.ellipsoid = size.divide(new BABYLON.Vector3(2, 2, 2)).subtract(new BABYLON.Vector3(0.001, 0.001, 0.001))
         this.shape.checkCollisions = true;
-        this.shape.material = params.mat
+        this.shape.material = trashTexture
+        
 
         this.weight = params.weight
     }
